b1fe9e037f5caa8466d6123af083691bb6b7f400
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-29 17:28:27 -0300

    firebase - create new components SignUp e Login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 import firebase from "./firebaseConfig";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Login from "./Login";
+import SignUp from "./SignUp";
+// import Home from "./Home";
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -13,7 +19,16 @@ class App extends React.Component {
         <header className="App-header">
           <h1>#partiuBurgerQueen</h1>
         </header>
+        <Router>
+          <div>
+            {/* <Route exact path="/" component={Home} /> */}
+            {/* <Route exact path="/login" component={Login} /> */}
+            <Route exact path="/signup" component={SignUp} />
+          </div>
+        </Router>
       </div>
+
+
     );
   }
 }
