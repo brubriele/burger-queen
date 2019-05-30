4003d9e247398b12390003fd3de39f166f5589c7
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-30 02:34:56 -0300

    testes-react - ícones kitchen/saloon, install boostrap

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 // import Home from "./Home";
 import imgBanner from './assets/img/burger_queen_gif.gif'
+import iconSaloon from './assets/img/pedidos-pic-salao.png'
+import iconKitchen from './assets/img/kitchen-pic.png'
+
 
 
 class App extends React.Component {
@@ -20,6 +23,10 @@ class App extends React.Component {
         <header className="App-header">
           <h1>#partiuBurgerQueen??</h1>
           <img src={imgBanner} alt="Logo" />
+          <div className="App-menu-container">
+            <img src={iconSaloon} alt="Logo" />
+            <img src={iconKitchen} alt="Logo" />
+          </div>
           <Router>
             <div>
               <Route exact path="/signup" component={SignUp} />
