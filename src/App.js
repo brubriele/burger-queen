a051e92538336864e6aca9e0898a4c9a89033288
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-29 23:07:14 -0300

    testes-react - teste para import img

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Login from "./Login";
 import SignUp from "./components/SignUp";
 // import Home from "./Home";
+import imgBanner from './assets/img/burger_queen_gif.gif'
 
 
 class App extends React.Component {
@@ -18,6 +19,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1>#partiuBurgerQueen??</h1>
+          <img src={imgBanner} alt="Logo" />
           <Router>
             <div>
               <Route exact path="/signup" component={SignUp} />
