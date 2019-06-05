fd9cabb6b34ab2d6838750a3768e96f30a5fdbf4
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-06-05 18:58:48 -0300

    testes-refact - implementa home com forma para cadastro e login, closes #3

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import firebase from "./firebaseConfig";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Login from "./Login";
 import SignUp from "./components/SignUp";
-// import Home from "./Home";
+import Home from "./components/Home";
 import imgBanner from './assets/img/burger_queen_gif.gif'
 import iconSaloon from './assets/img/pedidos-pic-salao.png'
 import iconKitchen from './assets/img/kitchen-pic.png'
-
-const database = firebase.firestore()
 
 
 
@@ -23,12 +21,14 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
+         
           <h1>#partiuBurgerQueen??</h1>
           <img src={imgBanner} alt="Logo" />
           <div className="App-menu-container">
             <img src={iconSaloon} alt="Logo" />
             <img src={iconKitchen} alt="Logo" />
           </div>
+          <Home />
           <Router>
             <div>
               <Route exact path="/signup" component={SignUp} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React from 'react';
+import Button from './Button'
+import firebase from '../firebaseConfig';
+import withFirebaseAuth from 'react-with-firebase-auth';
 
-const Home = () => {
-  return <h1>Home</h1>;
-};
+const firebaseAppAuth = firebase.auth();
 
-export default Home;
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      senha: "",
+    };
+  }
+
+  handleChange = (event, element) => {
+    const newState = this.state;
+    newState[element] = event.target.value
+    this.setState(newState);
+  }
+
+  createUser = () => {
+    this.props.createUserWithEmailAndPassword(this.state.email, this.state.senha);
+  }
+
+  signIn = () => {
+    this.props.signInWithEmailAndPassword(this.state.email, this.state.senha)
+      .then(() => {
+        alert("uhul");
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        
+        <input value={this.state.email}
+          placeholder="email"
+          onChange={(e) => this.handleChange(e, "email")} />
+        <input value={this.state.senha}
+          placeholder="senha"
+          onChange={(e) => this.handleChange(e, "senha")} />
+        <Button text="criar usuário" onClick={this.createUser} />
+        <Button text="loga com usuario" onClick={this.signIn} />
+      </div>
+    )
+  }
+}
+
+export default withFirebaseAuth({
+  firebaseAppAuth,
+})(Home);
