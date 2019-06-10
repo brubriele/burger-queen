b44a93e4aa090c66f419cd319347589707f36249
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-06-10 15:06:03 -0300

    teste-router - create user in collection

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,16 @@ import firebase from '../firebaseConfig';
 import withFirebaseAuth from 'react-with-firebase-auth';
 
 const firebaseAppAuth = firebase.auth();
+const database = firebase.firestore();
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      nome: "",
       email: "",
       senha: "",
+      perfil: "--selecione--",
     };
   }
 
@@ -22,7 +25,14 @@ class Home extends React.Component {
   }
 
   createUser = () => {
-    this.props.createUserWithEmailAndPassword(this.state.email, this.state.senha);
+    this.props.createUserWithEmailAndPassword(this.state.email, this.state.senha)
+      .then(resp => {
+        const id = resp.user.uid;
+        database.collection("burger-queen-users").doc(id).set({
+          email: this.state.email,
+          nome: this.state.nome
+        });
+      });
   }
 
   signIn = () => {
@@ -33,43 +43,44 @@ class Home extends React.Component {
   }
 
   render() {
+    console.log(this.state)
     return (
 
       <div className="Home-header">
 
 
-          <div className="dashboard">
-            <div className="dashboard-card main">
-              <div className="dashboard-card-content">
-                <form className="pure-form">
-                  <fieldset>
-                    <input value={this.state.nome} className="pure-form item" type="text" placeholder="Nome" onChange={(e) => this.handleChange(e, "nome")} />
-                    <input value={this.state.email} className="pure-form item" onChange={(e) => this.handleChange(e, "email")} type="email" placeholder="Email" />
-                    <input value={this.state.senha}
-                      placeholder="senha"
-                      onChange={(e) => this.handleChange(e, "senha")} className="pure-form item" type="password" placeholder="Senha" />
+        <div className="dashboard">
+          <div className="dashboard-card main">
+            <div className="dashboard-card-content">
+              <form className="pure-form">
+                <fieldset>
+                  <input value={this.state.nome} className="pure-form item" type="text" placeholder="Nome" onChange={(e) => this.handleChange(e, "nome")} />
+                  <input value={this.state.email} className="pure-form item" onChange={(e) => this.handleChange(e, "email")} type="email" placeholder="Email" />
+                  <input value={this.state.senha}
+                    placeholder="senha"
+                    onChange={(e) => this.handleChange(e, "senha")} className="pure-form item" type="password" placeholder="Senha" />
 
-                    <select>
-                      <option value="" disabled selected>--selecione--</option>
-                      <option>SALÃO</option>
-                      <option>COZINHA</option>
-                    </select>
+                  <select onChange={(e) => this.handleChange(e, "perfil")}>
+                    <option value="">--selecione--</option>
+                    <option>SALÃO</option>
+                    <option>COZINHA</option>
+                  </select>
 
-                    <div>
-                      <button text="Entrar" onClick={this.signIn} className="button-warning pure-button">Entrar</button>
-                      <button text="Novo Usuário" onClick={this.createUser} className="button-warning pure-button">Cadastrar</button>
+                  <div>
+                    <button text="Entrar" onClick={this.signIn} className="button-warning pure-button">Entrar</button>
+                    <button text="Novo Usuário" onClick={this.createUser} className="button-warning pure-button">Cadastrar</button>
 
-                    </div>
-
-
-                  </fieldset>
-                </form>
+                  </div>
 
 
-              </div>
+                </fieldset>
+              </form>
+
+
             </div>
           </div>
         </div>
+      </div>
 
 
     )
@@ -78,4 +89,5 @@ class Home extends React.Component {
 
 export default withFirebaseAuth({
   firebaseAppAuth,
+  database
 })(Home);
