156ece0dd23f489d3680fbae5a3613c62ae15a43
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-06-19 11:42:35 -0300

    redirect - implementa tab para menu e map para buttons, closes #12

--- src/pages/Saloon.js
+++ src/pages/Saloon.js
@@ -3,14 +3,71 @@ import 'purecss/build/pure.css';
 import './Saloon.css';
 import firebase from '../firebaseConfig';
 
+const menu = [
+    {
+        nome: "Café americano",
+        preco: 5,
+        período: "manhã"
+    },
+    {
+        nome: "Sanduíche de presunto e queijo",
+        preco: 10,
+        período: "manhã"
+    },
+    {
+        nome: "Suco de fruta natural",
+        preco: 7,
+        tipo: "manhã"
+    },
+    {
+        nome: "Hambúrguer simples",
+        preco: 10,
+        tipo: "diário"
+    },
+    {
+        nome: "Hambúrguer duplo",
+        preco: 15,
+        tipo: "diário"
+    },
+    {
+        nome: "Batata frita",
+        preco: 5,
+        tipo: "diário"
+    },
+    {
+        nome: "Anéis de cebola",
+        preco: 5,
+        tipo: "diário"
+    },
+    {
+        nome: "Água 500ml",
+        preco: 5,
+        tipo: "bebidas"
+    },
+    {
+        nome: "Água 750ml",
+        preco: 7,
+        tipo: "bebidas"
+    },
+    {
+        nome: "Bebida gaseificada 500ml",
+        preco: 7,
+        tipo: "bebidas"
+    },
+    {
+        nome: "Bebida gaseificada 750ml",
+        preco: 10,
+        tipo: "bebidas"
+    }
+]
+
 class Saloon extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            pedido: "",
-            mesa: "",
+            comprar: []
         };
-    };
+    }
 
     handleChange = (event, element) => {
         const newState = this.state;
@@ -18,42 +75,64 @@ class Saloon extends React.Component {
         this.setState(newState);
     }
 
-    addItem = () => {
-
+    buyItem = (item) => {
+        
     }
 
     render() {
+        console.log(this.state.comprar)
         return (
             <div className="Pannel-header">
-                <div class="pc-tab">
+                <div className="pc-tab">
                     <input checked="checked" id="tab1" type="radio" name="pct" />
                     <input id="tab2" type="radio" name="pct" />
                     <input id="tab3" type="radio" name="pct" />
                     <nav>
                         <ul>
-                            <li class="tab1">
+                            <li className="tab1">
                                 <label for="tab1">Café da Manhã</label>
                             </li>
-                            <li class="tab2">
-                                <label for="tab2">Lanches</label>
+                            <li className="tab2">
+                                <label for="tab2">Menu Diário</label>
                             </li>
                         </ul>
                     </nav>
                     <section>
-                        <div class="tab1">
-                            <h2>First</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus itaque quidem minus nostrum, voluptatem accusamus aspernatur quia harum ratione, officia laudantium inventore autem doloribus atque labore numquam non. Hic, animi.</p>
+                        <div className="tab1">
+                            <h2>Café da Manhã</h2>
+                            {
+                                menu.map((menu, i) => {
+                                    return menu.tipo === 'manhã' ?
+                                    <button className="button-xsmall pure-button" key={i} onClick={() => {alert (menu.nome)}}>{menu.nome} <br></br>{'R$' + menu.preco}</button> : false
+                                }) 
+                            }
+
+
                         </div>
-                        <div class="tab2">
-                            <h2>Second</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum nesciunt ipsum dolore error repellendus officiis aliquid a, vitae reprehenderit, accusantium vero, ad. Obcaecati numquam sapiente cupiditate. Praesentium eaque, quae error!</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, maiores.</p>
+                        <div className="tab2">
+                            <h2>Menu Diário</h2>
+                            {
+                                menu.map((menu, i) => {
+                                    return menu.tipo === 'diário' ?
+                                    <button className="button-xsmall pure-button" key={i} onClick={() => {alert (menu.nome)}}>{menu.nome} <br></br>{"R$" + menu.preco}</button> : false
+                                }) 
+                            }
+                        </div>
+                    </section>
+                    <section>
+                    <div class="tab1">
+                            <h2>Total pedido:</h2>
+                        </div>
+                    </section>
+                    <section>
+                    <div class="tab2">
+                            <h2>Total pedido:</h2>
                         </div>
                     </section>
                 </div>
             </div>
         )
-        
+
     }
 }
 
